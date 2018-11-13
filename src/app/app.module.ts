b96cfad7c807fa784cc36b857e3f3fea714a2952
Paulo Serafim <paserafim@gmail.com>
2018-11-13 22:51:52 +0000

    angular material components initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { CoreModule } from './core/core.module';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularMaterialModule } from './../angular-material/angular-material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [ToolbarComponent],
+  imports: [
+    CommonModule,
+    AngularMaterialModule
+
+  ],
+  exports: [
+    ToolbarComponent
+  ]
+})
+export class CoreModule { }
